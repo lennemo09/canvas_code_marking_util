45ebc00324a168b4cdfdc177f8b4efcf78760206
Len <lennemo09@gmail.com>
2022-12-02 19:06:44 +0700

    Added Timeout handling for infinitely running submissions.

--- predefined_solutions.py
+++ predefined_solutions.py
@@ -19,16 +19,16 @@ solution_g1_q3a = Solution(file_path=".\solution\g1-q3a.py", question_number="3a
 solution_g1_q3a.inputs_to_test = [[1,3,4,5],
                                 [-2,0,2,4,6,8],
                                 [1,-3,3,7]]
-solution_g1_q3a.functions_to_test = "odds_sum"
+solution_g1_q3a.function_to_test = "odds_sum"
 
 # Question 3b
 solution_g1_q3b = Solution(file_path=".\solution\g1-q3b.py", question_number="3b", group_number='1', run_from_main=False)
 solution_g1_q3b.inputs_to_test = [4, 8, -27, 0, 27, 1331, -1331]
-solution_g1_q3b.functions_to_test = "cube_root"
+solution_g1_q3b.function_to_test = "cube_root"
 
 # Question 4
 solution_g1_q4 = Solution(file_path=".\solution\g1-q4.py", question_number="4", group_number='1', run_from_main=False, multi_input=True)
-solution_g1_q4.functions_to_test = "add_matrix"
+solution_g1_q4.function_to_test = "add_matrix"
 solution_g1_q4.inputs_to_test = [[[[1,2,3], [1,5,1], [1,2,2]], [[3,1,0], [1,1,2], [2,1,0]]],
                             [[[4, 3, 3], [2, 6, 3], [3, 3, 2]], [[1,2,3], [1,5,1], [1,2,2]]],
                             [[[1,2],[2,3],[3,4]], [[5,6],[7,8],[9,10]]]]
@@ -36,11 +36,47 @@ solution_g1_q4.inputs_to_test = [[[[1,2,3], [1,5,1], [1,2,2]], [[3,1,0], [1,1,2]
 
 ############### GROUP 2 ##############
 
+# Question 1
+solution_g2_q1 = Solution(file_path=".\solution\g2-q1.py", question_number="1", group_number='2', run_from_main=True)
+solution_g2_q1.inputs_to_test = ["VinUni is a yOUng institution\nin Vietnam\n", 
+                                "Hi my name is Jack\nthe reaper\n",
+                                "I really really like pancakes\nwith bacon\n"]
+
+# Question 2
+solution_g2_q2 = Solution(file_path=".\solution\g2-q2.py", question_number="2", group_number='2', run_from_main=False)
+solution_g2_q2.inputs_to_test = [10,150,300,500]
+solution_g2_q2.function_to_test = "electricity_cost"
+
+# Question 3a
+solution_g2_q3a = Solution(file_path=".\solution\g2-q3a.py", question_number="3a", group_number='2', run_from_main=False)
+solution_g2_q3a.inputs_to_test = [[1,2,3,4,5,6],
+                                [-1],
+                                [0,-10,0,5,5]]
+solution_g2_q3a.function_to_test = "cummulative_list"
+
+# Question 3b
+solution_g2_q3b = Solution(file_path=".\solution\g2-q3b.py", question_number="3b", group_number='2', run_from_main=False, multi_input=True)
+solution_g2_q3b.inputs_to_test = [["abc",1],
+                                ["abcd",-4],
+                                ["1234!! hello world?",8]]
+solution_g2_q3b.function_to_test = "caesar"
+
+# Question 4
+solution_g2_q4 = Solution(file_path=".\solution\g2-q4.py", question_number="4", group_number='2', run_from_main=False, multi_input=True)
+solution_g2_q4.function_to_test = "multiply_matrix"
+solution_g2_q4.inputs_to_test = [[[[1,2,3], [1,5,1], [1,2,2]], [[3,1,0], [1,1,2], [2,1,0]]],
+                            [[[4, 3, 3], [2, 6, 3], [3, 3, 2]], [[1,2,3], [1,5,1], [1,2,2]]],
+                            [[[1,2],[2,3],[3,4]], [[5,6],[7,8],[9,10]]]]
 
 solution_dict = {
     ('1','1') : solution_g1_q1,
     ('2','1') : solution_g1_q2,
     ('3a','1'): solution_g1_q3a,
     ('3b','1'): solution_g1_q3b,
-    ('4','1') : solution_g1_q4
+    ('4','1') : solution_g1_q4,
+    ('1','2') : solution_g2_q1,
+    ('2','2') : solution_g2_q2,
+    ('3a','2'): solution_g2_q3a,
+    ('3b','2'): solution_g2_q3b,
+    ('4','2') : solution_g2_q4
 }
--- solution.py
+++ solution.py
@@ -2,9 +2,10 @@ from runnable import Runnable
 from submission import Submission
 import random, subprocess, sys, os
 import traceback
+from func_timeout import func_timeout, FunctionTimedOut
 
 random.seed(1337)
-
+RUNTIME_LIMIT = 1 # Seconds
 
 class Solution(Runnable):
     def __init__(self, file_path, question_number, group_number, function_to_test = None, inputs_to_test = [], run_from_main=False, multi_input=False):
@@ -24,7 +25,7 @@ class Solution(Runnable):
         """
         super().__init__(file_path, question_number, group_number)
 
-        self.functions_to_test = function_to_test
+        self.function_to_test = function_to_test
         self.inputs_to_test = inputs_to_test
         
         self.run_from_main = run_from_main
@@ -48,31 +49,31 @@ class Solution(Runnable):
             solution_output_string = ""
             if self.run_from_main: # When the question module's content is not wrapped in a function.
                 for test_input in self.inputs_to_test:
-                    solution_output_string += "\n####################################"
+                    solution_output_string += "####################################"
                     solution_output_string += f"\nTesting with inputs:\n{test_input}"                    
-                    solution_subprocess = subprocess.run(['python', self.file_path], text=True, capture_output=True, input=test_input)
+                    solution_subprocess = subprocess.run(['python', self.file_path], text=True, timeout=RUNTIME_LIMIT, capture_output=True, input=test_input)
                     solution_output_string += "\n\nOutput from solution:\n"
                     solution_output_string += solution_subprocess.stdout
 
-                    submission_output_string += "\n####################################"
+                    submission_output_string += "####################################"
                     submission_output_string += f"\nTesting with inputs:\n{test_input}"
-                    submission_subprocess = subprocess.run(['python', submission.file_path], text=True, capture_output=True, input=test_input)
+                    submission_subprocess = subprocess.run(['python', submission.file_path], text=True, timeout=RUNTIME_LIMIT, capture_output=True, input=test_input)
                     submission_output_string += f"\nOutput match solution: {submission_subprocess.stdout == solution_subprocess.stdout}"
                     submission_output_string += "\n\nOutput from submission:\n"
                     submission_output_string += submission_subprocess.stdout
 
             else: # When the question module's content is wrapped in a function.
-                submission_module = submission.load_module()
+                submission_module = func_timeout(RUNTIME_LIMIT, submission.load_module)
                 solution_module = self.load_module()
 
-                solution_function = getattr(solution_module, self.functions_to_test)
-                submission_function = getattr(submission_module, self.functions_to_test)
+                solution_function = getattr(solution_module, self.function_to_test)
+                submission_function = getattr(submission_module, self.function_to_test)
 
                 for test_input in self.inputs_to_test:
                     if suppress_stdout:
                         sys.stdout = open(os.devnull, 'w') # Suppresses stdout from loaded module.
                     
-                    solution_output_string += "\n####################################"
+                    solution_output_string += "####################################"
                     solution_output_string += f"\nTesting with inputs:\n{test_input}"
 
                     if not self.multi_input: # If the function only takes 1 argument
@@ -84,13 +85,13 @@ class Solution(Runnable):
                     solution_output_string += solution_output.__repr__()
 
 
-                    submission_output_string += "\n####################################"
+                    submission_output_string += "####################################"
                     submission_output_string += f"\nTesting with inputs:\n{test_input}"
 
                     if not self.multi_input: # If the function only takes 1 argument
-                        submission_output = submission_function(test_input)
+                        submission_output = func_timeout(RUNTIME_LIMIT, submission_function, args=(test_input))
                     else: # If the function takes in multiple arguments -> unpack the list of inputs
-                        submission_output = submission_function(*test_input)
+                        submission_output = func_timeout(RUNTIME_LIMIT, submission_function, args=tuple(test_input))
 
                     submission_output_string += f"\nOutput match solution: {submission_output == solution_output}"
                     submission_output_string += "\n\nOutput from submission:\n"
@@ -98,7 +99,18 @@ class Solution(Runnable):
 
                     if suppress_stdout:
                         sys.stdout = sys.__stdout__# Unsuppresses stdout from loaded module.
-                    
+        except FunctionTimedOut:
+            if suppress_stdout:
+                sys.stdout = sys.__stdout__# Unsuppresses stdout from loaded module.
+
+            submission_output_string += "\n\nSubmission timeout! Took too long to run.\n\n"
+            submission_output_string += traceback.format_exc()
+        except subprocess.TimeoutExpired:
+            if suppress_stdout:
+                sys.stdout = sys.__stdout__# Unsuppresses stdout from loaded module.
+
+            submission_output_string += "\n\nSubmission timeout! Took too long to run.\n\n"
+            submission_output_string += traceback.format_exc()
         except Exception as e:
             if suppress_stdout:
                 sys.stdout = sys.__stdout__# Unsuppresses stdout from loaded module.
@@ -129,11 +141,11 @@ if __name__ == "__main__":
     solution_g1_q2.inputs_to_test = ["5\n20\n80\n100\n90\n50\n"]
 
     solution_g1_q3a = Solution(file_path=".\solution\g1-q3a.py", question_number="3a", group_number=1, run_from_main=False)
-    solution_g1_q3a.functions_to_test = "odds_sum"
+    solution_g1_q3a.function_to_test = "odds_sum"
     solution_g1_q3a.inputs_to_test = [[1,3,4,5],[-2,0,2,4,6,8],[1,-3,3,7]]
 
     solution_g1_q4 = Solution(file_path=".\solution\g1-q4.py", question_number="4", group_number=1, run_from_main=False, multi_input=True)
-    solution_g1_q4.functions_to_test = "add_matrix"
+    solution_g1_q4.function_to_test = "add_matrix"
     solution_g1_q4.inputs_to_test = [[[[1,2,3], [1,5,1], [1,2,2]], [[3,1,0], [1,1,2], [2,1,0]]],
                                 [[[4, 3, 3], [2, 6, 3], [3, 3, 2]], [[1,2,3], [1,5,1], [1,2,2]]],
                                 [[[1,2],[2,3],[3,4]], [[5,6],[7,8],[9,10]]]]
